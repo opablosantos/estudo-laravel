5ce31f29ff4e11696c30e0cad4f77971bc0cb796
Pablo Santos <opablosantos@gmail.com>
2021-07-13 20:52:08 -0300

    Iniciando filtering

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -19,7 +19,7 @@
                 <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
 
                 @foreach ($categories as $category)
-                <x-dropdown-item href="/categories/{{ $category->slug }}" :active="request()->is('categories/' . $category->slug)">{{ ucwords($category->name) }}</x-dropdown-item>
+                <x-dropdown-item href="/?category={{ $category->slug }}" :active="request()->is('categories/' . $category->slug)">{{ ucwords($category->name) }}</x-dropdown-item>
                 @endforeach
             </x-dropdown>
         </div>
