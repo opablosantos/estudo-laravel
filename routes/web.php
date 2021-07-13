95888906b71c0865195e78d3a29d9086e29fa525
Pablo Santos <opablosantos@gmail.com>
2021-07-12 21:32:05 -0300

    Finalizando a sessão 5
    Finalizando a sessão de integração com o design

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/', function () {
         'categories' => Category::all()
     ]);
 
-});
+})->name('home');
 
 Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
@@ -24,7 +24,7 @@ Route::get('posts/{post:slug}', function (Post $post) {
 
 Route::get('categories/{category:slug}', function(Category $category) {
     return view('posts', ['posts' => $category->posts, 'currentCategory' => $category, 'categories' => Category::all()]);
-});
+})->name('category');
 
 Route::get('authors/{author:username}', function(User $author) {
     return view('posts', ['posts' => $author->posts, 'categories' => Category::all()]);
