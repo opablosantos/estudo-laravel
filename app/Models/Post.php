fbad3239a7a3639eed905085c2713a7b29755ccd
Pablo Santos <opablosantos@gmail.com>
2021-07-14 18:52:20 -0300

    Finalizando filtering

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -17,9 +17,10 @@ class Post extends Model
     public function scopeFilter($query, array $filters)
     {
         $query->when($filters['search'] ?? false, function($query, $search) {
-            $query
-                ->where('title', 'like', '%' . $search . '%')
+            $query->where(function($query) use($search) {
+                $query->where('title', 'like', '%' . $search . '%')
                 ->orWhere('body', 'like', '%' . $search . '%');
+            });
         });
 
         $query->when($filters['category'] ?? false, function($query, $category)  {
--- resources/views/posts/_header.blade.php
+++ resources/views/posts/_header.blade.php
@@ -1,6 +1,6 @@
 <header class="max-w-xl mx-auto mt-20 text-center">
     <h1 class="text-4xl">
-        Latest <span class="text-blue-500">Laravel From Scratch</span> News
+        Curso <span class="text-blue-500">Laravel From Scratch</span> com Jeffrey Way
     </h1>
 
     <h2 class="inline-flex mt-2 font-bold">By Pablo Santos  <img src="/images/pablo.png"></h2>
