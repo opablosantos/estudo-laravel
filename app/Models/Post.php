53dee27c3cf18bf86305080c3d193a0167104502
Pablo Santos <opablosantos@gmail.com>
2021-07-14 17:38:49 -0300

    Merge category and search

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -27,6 +27,12 @@ class Post extends Model
                 $query->where('slug', $category);
             });
         });
+
+        $query->when($filters['author'] ?? false, function($query, $author)  {
+            $query->whereHas('author', function($query) use ($author) {
+                $query->where('username', $author);
+            });
+        });
     }
 
     public function category()
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\Category;
-use App\Models\Post;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Log;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
-
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
-
-Route::get('authors/{author:username}', function(User $author) {
-    return view('posts', ['posts' => $author->posts]);
-});
