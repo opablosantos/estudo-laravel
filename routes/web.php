da4c5e59e4010a11e1e414c5d47884e6c350c3e6
Pablo Santos <opablosantos@gmail.com>
2021-07-14 16:32:42 -0300

    Alterações no código ep 40

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('authors/{author:username}', function(User $author) {
-    return view('posts', ['posts' => $author->posts, 'categories' => Category::all()]);
+    return view('posts', ['posts' => $author->posts]);
 });
