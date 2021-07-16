1f619b159b3b9b653c65345154225ec646a54a68
Pablo Santos <opablosantos@gmail.com>
2021-07-16 17:34:56 -0300

    Mostrando mensagem de erro pro usuário

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -10,7 +10,11 @@
                         Name
                     </label>
 
-                    <input class="border border-gray-400 p-2 w-full" type="text" name="name" id="name" required>
+                    <input class="border border-gray-400 p-2 w-full" type="text" name="name" id="name" value="{{ old('name') }}" required>
+
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -18,7 +22,11 @@
                         Username
                     </label>
 
-                    <input class="border border-gray-400 p-2 w-full" type="text" name="username" id="username" required>
+                    <input class="border border-gray-400 p-2 w-full" type="text" name="username" id="username" value="{{ old('username') }}" required>
+
+                    @error('username')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -26,7 +34,11 @@
                         Email
                     </label>
 
-                    <input class="border border-gray-400 p-2 w-full" type="email" name="email" id="email" required>
+                    <input class="border border-gray-400 p-2 w-full" type="email" name="email" id="email" value="{{ old('email') }}" required>
+
+                    @error('email')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -35,6 +47,10 @@
                     </label>
 
                     <input class="border border-gray-400 p-2 w-full" type="password" name="password" id="password" required>
+
+                    @error('password')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -42,6 +58,13 @@
                         Submit
                     </button>
                 </div>
+                {{-- @if ($errors->any())
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li class="text-red-500 text-xs">{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                @endif --}}
             </form>
         </main>
     </section>
