12d4a304e123db49af0c1d2dab8715b26164807f
Pablo Santos <opablosantos@gmail.com>
2021-07-16 19:29:30 -0300

    Trabalhando com sessões

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades;
 
 class RegisterController extends Controller
 {
@@ -21,7 +22,9 @@ class RegisterController extends Controller
             'password' => 'required|min:7|max:255'
         ]);
 
-        User::create($attributes);
+        $user = User::create($attributes);
+
+        auth()->login($user);
 
         return redirect('/')->with('success', 'Your account has been created.');
     }
